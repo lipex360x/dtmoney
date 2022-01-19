38cc850dfaeade5ad2f4aaa76a1ac1aeaf982fce
Felipe Borges <lipex360@gmail.com>
2022-01-18 22:33:07 -0300

    modal style updated

--- frontend/src/components/NewTransaction/index.tsx
+++ frontend/src/components/NewTransaction/index.tsx
@@ -1,6 +1,9 @@
-// import * as S from './styles'
+import * as S from './styles'
 import Modal from 'react-modal'
+
 import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 
 interface NewTransactionProps {
   isOpen: boolean
@@ -18,6 +21,41 @@ export function NewTransaction ({ isOpen, onRequestClose }:NewTransactionProps) 
       <button className='btn-modal-close' onClick={onRequestClose}>
         <img src={closeImg} alt="" />
       </button>
+    <S.Container>
+      <h2>New Transaction</h2>
+
+      <input
+        type="text"
+        placeholder='Name'
+      />
+
+      <input
+        type="text"
+        placeholder='value'
+      />
+
+      <S.TypeButtons>
+
+        <S.Button>
+          <img src={incomeImg} alt="Income" />
+          <span>Income</span>
+        </S.Button>
+
+        <S.Button>
+          <img src={outcomeImg} alt="Outcome" />
+          <span>Outcome</span>
+        </S.Button>
+
+      </S.TypeButtons>
+
+      <input
+        type="text"
+        placeholder='category'
+      />
+
+      <button type='submit'>Save Transaction</button>
+
+    </S.Container>
     </Modal>
   )
 }
--- frontend/src/components/NewTransaction/styles.ts
+++ frontend/src/components/NewTransaction/styles.ts
@@ -1,5 +1,70 @@
 import styled from 'styled-components'
+import { darken } from 'polished'
 
-export const Container = styled.section`
+export const Container = styled.form`
+ display: grid;
+ gap: 1rem;
 
+ h2 {
+   font-size: 1.5rem;
+   font-weight: 500;
+   color: var(--text-title)
+ }
+
+ input[type='text'] {
+   font-size: 1rem;
+   border: 1px solid #D7D7D7;
+   border-radius: 0.25rem;
+   padding: 1rem;
+
+   &::placeholder {
+     color: #E7E9EE;
+   }
+
+   &:focus {
+      border: 1px solid #D7D7D7;
+      outline: 1px solid #d0d0d0;
+   }
+ }
+
+ button[type='submit'] {
+    font-size: 1rem;
+    padding: 1rem;
+    background: var(--green);
+    border-radius: 0.25rem;
+    color: #fff;
+    font-weight: 400;
+    transition: background 0.3s;
+
+    &:hover {
+      background: ${darken(0.05, '#33cc95')};
+    }
+ }
+`
+
+export const TypeButtons = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1rem;
+`
+
+export const Button = styled.button`
+  display: flex;
+  height: 3.5rem;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+
+  img {
+    width: 20px;
+    height: 20px
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title)
+  }
 `
