7ef657f8d98601dbc2d04129209b5d9b5831eaca
Felipe Borges <lipex360@gmail.com>
2022-01-20 12:18:14 -0300

    plop updated

--- frontend/src/shared/generators/plopfile.js
+++ frontend/src/shared/generators/plopfile.js
@@ -1,7 +1,9 @@
 const components = require('./components')
+const context = require('./context')
 const generator = require('./_generator')
 
 module.exports = function (plop) {
   plop.setGenerator('Components', components)
+  plop.setGenerator('Contexts', context)
   plop.setGenerator('Generator', generator)
 }
